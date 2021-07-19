7db15e213b0e7e6247f5733ead041ffff092170d
zihao <1819851507@qq.com>
2021-07-19 22:01:01 +0800

    3

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -34,47 +34,681 @@
 					indicator-color='#fff'
 					indicator-active-color='#0D92FF'>
 				<swiper-item>
-					<view style="width: auto;height: 300px;background-color: #007AFF;">A</view>
+					<view style="width: auto;height: 240rpx;background-color: #007AFF;">A</view>
 				</swiper-item>
 				<swiper-item>
-					<view style="width: auto;height: 300px;background-color: #3F536E;">B</view>
+					<view style="width: auto;height: 240rpx;background-color: #3F536E;">B</view>
 				</swiper-item>
 				<swiper-item>
-					<view style="width: auto;height: 300px;background-color: #4CD964;">C</view>
+					<view style="width: auto;height: 240rpx;background-color: #4CD964;">C</view>
 				</swiper-item>
 			</swiper>
 		</view>
 		
 		
-		<view class="">
-			<view class="">
-				<view class="">
+		<view class="Merit">
+			<view class="Merit_item">
+				<view class="Merit_icon">
 					
 				</view>
-				<view class="title">
+				<view class="Merit_name">
 					团餐
 				</view>
 			</view>
 			
-			<view class="">
-				<view class="">
+			<view class="Merit_item">
+				<view class="Merit_icon">
 					
 				</view>
-				<view class="title">
+				<view class="Merit_name">
 					拼餐
 				</view>
 			</view>
 			
-			<view class="">
-				<view class="">
+			<view class="Merit_item">
+				<view class="Merit_icon">
 					
 				</view>
-				<view class="title">
+				<view class="Merit_name">
 					贵宾
 				</view>
 			</view>
 		</view>
 		
+		<view class="screen_box">
+			<view class="screen_left">
+				<view class="select_item">
+					午餐
+				</view>
+				<view class="select_item">
+					综艺排序
+				</view>
+				<view class="select_item">
+					距离
+				</view>
+				<view class="select_item">
+					销量
+				</view>
+			</view>
+			<view class="screen">
+				筛选
+			</view>
+		</view>
+		<view class="setMeal_box">
+			<view class="setMeal_item">
+				行政套餐
+			</view>
+			<view class="setMeal_item">
+				自选套餐
+			</view>
+			<view class="setMeal_item">
+				会务套餐
+			</view>
+		</view>
+		
+		<view class="label_box">
+			<view class="label_item">
+				香辣川味
+			</view>
+			<view class="label_item">
+				清淡粤菜
+			</view>
+			<view class="label_item">
+				精致甜品
+			</view>
+			<view class="label_item">
+				水果配送
+			</view>
+		</view>
+		<view class="good_all_box">
+			<view class="good_item_box">
+				<view class="googImgBox">
+					
+				</view>
+				<view class="good_item_describe">
+					<view class="good_name_mueu">
+						<view class="good_name">
+							111111
+						</view>
+						<view class="good_mueu">
+							...
+						</view>
+					</view>
+					
+					<view class="good_describe">
+						<view class="good_score">
+							1
+						</view>
+						<view class="good_sale">
+							2
+						</view>
+					</view>
+					
+					<view class="good_performance">
+						<view class="good_start_price">
+							1
+						</view>
+						<view class="good_distance_time">
+							1
+						</view>
+					</view>
+					
+					<view class="good_describe_label_box">
+						<view class="good_item_describe_label">
+							配送
+						</view>
+						<view class="good_item_describe_label">
+							配送
+						</view>
+						<view class="good_item_describe_label">
+							配送
+						</view>
+						<view class="good_item_describe_label">
+							配送
+						</view>
+					</view>
+					<view class="good_taste_label_box">
+						<view class="good_item_taste_label_box">
+							香辣
+						</view>
+						<view class="good_item_taste_label_box">
+							香辣
+						</view>
+						<view class="good_item_taste_label_box">
+							香辣
+						</view>
+						<view class="good_reserve">
+							预定
+						</view>
+							
+					</view>
+					
+					
+					
+				</view>
+			</view>
+				
+			
+			<view class="good_item_box">
+				<view class="googImgBox">
+					
+				</view>
+				<view class="good_item_describe">
+					<view class="good_name_mueu">
+						<view class="good_name">
+							111111
+						</view>
+						<view class="good_mueu">
+							...
+						</view>
+					</view>
+					
+					<view class="good_describe">
+						<view class="good_score">
+							1
+						</view>
+						<view class="good_sale">
+							2
+						</view>
+					</view>
+					
+					<view class="good_performance">
+						<view class="good_start_price">
+							1
+						</view>
+						<view class="good_distance_time">
+							1
+						</view>
+					</view>
+					
+					<view class="good_describe_label_box">
+						<view class="good_item_describe_label">
+							配送
+						</view>
+						<view class="good_item_describe_label">
+							配送
+						</view>
+						<view class="good_item_describe_label">
+							配送
+						</view>
+						<view class="good_item_describe_label">
+							配送
+						</view>
+					</view>
+					<view class="good_taste_label_box">
+						<view class="good_item_taste_label_box">
+							香辣
+						</view>
+						<view class="good_item_taste_label_box">
+							香辣
+						</view>
+						<view class="good_item_taste_label_box">
+							香辣
+						</view>
+						<view class="good_reserve">
+							预定
+						</view>
+							
+					</view>
+					
+					
+					
+				</view>
+			</view>
+			
+			
+			
+		</view>
+		
+		<view class="member_box">
+			<view class="title">
+				会员专区
+			</view>
+			
+			<view class="bac_img">
+				<view class="bac_title">
+					品质午餐
+				</view>
+				<view class="food_box">
+					<view class="food_item">
+						<view class="food_img">
+							
+						</view>
+						<view class="food_describe">
+							<view class="food_name">
+								菜品名称
+							</view>
+							<view class="distance">
+								<view class="">
+									30分钟
+								</view>
+								<view class="">
+									1.4km
+								</view>
+								<view class="">
+									起送13
+								</view>
+							</view>
+							<view class="price">
+								56
+							</view>
+							<view class="shop_box">
+								<view class="shop">
+									<view class="shop_logo">
+										logo
+									</view>
+									<view class="shop_name">
+										店铺名称
+									</view>
+								</view>
+								<view class="evaluate">
+									4.8
+								</view>
+							</view>
+						</view>
+					</view>
+					
+					<view class="food_item">
+						<view class="food_img">
+							
+						</view>
+						<view class="food_describe">
+							<view class="food_name">
+								菜品名称
+							</view>
+							<view class="distance">
+								<view class="">
+									30分钟
+								</view>
+								<view class="">
+									1.4km
+								</view>
+								<view class="">
+									起送13
+								</view>
+							</view>
+							<view class="price">
+								56
+							</view>
+							<view class="shop_box">
+								<view class="shop">
+									<view class="shop_logo">
+										logo
+									</view>
+									<view class="shop_name">
+										店铺名称
+									</view>
+								</view>
+								<view class="evaluate">
+									4.8
+								</view>
+							</view>
+						</view>
+					</view>
+					
+				</view>
+			</view>
+		
+			<view class="bac_img">
+					<view class="bac_title">
+						品质午餐
+					</view>
+					<view class="food_box">
+						<view class="food_item">
+							<view class="food_img">
+								
+							</view>
+							<view class="food_describe">
+								<view class="food_name">
+									菜品名称
+								</view>
+								<view class="distance">
+									<view class="">
+										30分钟
+									</view>
+									<view class="">
+										1.4km
+									</view>
+									<view class="">
+										起送13
+									</view>
+								</view>
+								<view class="price">
+									56
+								</view>
+								<view class="shop_box">
+									<view class="shop">
+										<view class="shop_logo">
+											logo
+										</view>
+										<view class="shop_name">
+											店铺名称
+										</view>
+									</view>
+									<view class="evaluate">
+										4.8
+									</view>
+								</view>
+							</view>
+						</view>
+						
+						<view class="food_item">
+							<view class="food_img">
+								
+							</view>
+							<view class="food_describe">
+								<view class="food_name">
+									菜品名称
+								</view>
+								<view class="distance">
+									<view class="">
+										30分钟
+									</view>
+									<view class="">
+										1.4km
+									</view>
+									<view class="">
+										起送13
+									</view>
+								</view>
+								<view class="price">
+									56
+								</view>
+								<view class="shop_box">
+									<view class="shop">
+										<view class="shop_logo">
+											logo
+										</view>
+										<view class="shop_name">
+											店铺名称
+										</view>
+									</view>
+									<view class="evaluate">
+										4.8
+									</view>
+								</view>
+							</view>
+						</view>
+						
+					</view>
+				</view>
+			
+		
+		</view>
+		
+		
+		<view class="select_box">
+			<view class="setMeal_box">
+				<view class="setMeal_item">
+					附近商家
+				</view>
+				<view class="setMeal_item">
+					新品推荐
+				</view>
+			</view>
+			<view class="screen_box">
+				<view class="screen_left">
+					
+					<view class="select_item">
+						综艺排序
+					</view>
+					<view class="select_item">
+						距离
+					</view>
+					<view class="select_item">
+						销量
+					</view>
+				</view>
+				<view class="screen">
+					筛选
+				</view>
+			</view>
+			
+			<view class="label_box">
+				<view class="label_item">
+					香辣川味
+				</view>
+				<view class="label_item">
+					清淡粤菜
+				</view>
+				<view class="label_item">
+					精致甜品
+				</view>
+				<view class="label_item">
+					水果配送
+				</view>
+			</view>
+		
+			<view class="good_all_box">
+				<view class="goodBox">
+					
+				
+					<view class="good_item_box">
+						<view class="googImgBox">
+							
+						</view>
+						<view class="good_item_describe">
+							<view class="good_name_mueu">
+								<view class="good_name">
+									111111
+								</view>
+								<view class="good_mueu">
+									...
+								</view>
+							</view>
+							
+							<view class="good_describe">
+								<view class="good_score">
+									1
+								</view>
+								<view class="good_sale">
+									2
+								</view>
+							</view>
+							
+							<view class="good_performance">
+								<view class="good_start_price">
+									1
+								</view>
+								<view class="good_distance_time">
+									1
+								</view>
+							</view>
+							
+							<view class="good_describe_label_box">
+								<view class="good_item_describe_label">
+									配送
+								</view>
+								<view class="good_item_describe_label">
+									配送
+								</view>
+								<view class="good_item_describe_label">
+									配送
+								</view>
+								<view class="good_item_describe_label">
+									配送
+								</view>
+							</view>
+							<view class="good_taste_label_box">
+								<view class="good_item_taste_label_box">
+									香辣
+								</view>
+								<view class="good_item_taste_label_box">
+									香辣
+								</view>
+								<view class="good_item_taste_label_box">
+									香辣
+								</view>
+								<view class="good_reserve">
+									预定
+								</view>
+									
+							</view>
+							
+							
+							
+						</view>
+					</view>
+					<view class="good_mune_box">
+						<view class="good_item_mune_box">
+							
+							<view class="good_mune_img">
+								
+							</view>
+							<view class="good_mune_name">
+								菜品
+							</view>
+							<view class="good_mune_price">
+								56
+							</view>
+						</view>
+						<view class="good_item_mune_box">
+							
+							<view class="good_mune_img">
+								
+							</view>
+							<view class="good_mune_name">
+								菜品
+							</view>
+							<view class="good_mune_price">
+								56
+							</view>
+						</view>
+						<view class="good_item_mune_box">
+							
+							<view class="good_mune_img">
+								
+							</view>
+							<view class="good_mune_name">
+								菜品
+							</view>
+							<view class="good_mune_price">
+								56
+							</view>
+						</view>
+					</view>	
+				</view>
+				
+				<view class="goodBox">
+					
+				
+					<view class="good_item_box">
+						<view class="googImgBox">
+							
+						</view>
+						<view class="good_item_describe">
+							<view class="good_name_mueu">
+								<view class="good_name">
+									111111
+								</view>
+								<view class="good_mueu">
+									...
+								</view>
+							</view>
+							
+							<view class="good_describe">
+								<view class="good_score">
+									1
+								</view>
+								<view class="good_sale">
+									2
+								</view>
+							</view>
+							
+							<view class="good_performance">
+								<view class="good_start_price">
+									1
+								</view>
+								<view class="good_distance_time">
+									1
+								</view>
+							</view>
+							
+							<view class="good_describe_label_box">
+								<view class="good_item_describe_label">
+									配送
+								</view>
+								<view class="good_item_describe_label">
+									配送
+								</view>
+								<view class="good_item_describe_label">
+									配送
+								</view>
+								<view class="good_item_describe_label">
+									配送
+								</view>
+							</view>
+							<view class="good_taste_label_box">
+								<view class="good_item_taste_label_box">
+									香辣
+								</view>
+								<view class="good_item_taste_label_box">
+									香辣
+								</view>
+								<view class="good_item_taste_label_box">
+									香辣
+								</view>
+								<view class="good_reserve">
+									预定
+								</view>
+									
+							</view>
+							
+							
+							
+						</view>
+					</view>
+					<view class="good_mune_box">
+						<view class="good_item_mune_box">
+							
+							<view class="good_mune_img">
+								
+							</view>
+							<view class="good_mune_name">
+								菜品
+							</view>
+							<view class="good_mune_price">
+								56
+							</view>
+						</view>
+						<view class="good_item_mune_box">
+							
+							<view class="good_mune_img">
+								
+							</view>
+							<view class="good_mune_name">
+								菜品
+							</view>
+							<view class="good_mune_price">
+								56
+							</view>
+						</view>
+						<view class="good_item_mune_box">
+							
+							<view class="good_mune_img">
+								
+							</view>
+							<view class="good_mune_name">
+								菜品
+							</view>
+							<view class="good_mune_price">
+								56
+							</view>
+						</view>
+					</view>	
+				</view>
+				
+				
+				
+				
+			</view>
+		
+		</view>
+		
+		
+		<view class="ShoppingCart">
+			
+		</view>
 	</view>
 </template>
 
@@ -98,6 +732,10 @@
 </script>
 
 <style scoped>
+	
+	.content{
+		background:#F8F8F8;
+	}
 	.address{
 		position: absolute;
 		left: 25rpx;
@@ -109,6 +747,10 @@
 		align-items: center;
 		justify-content: center;
 		font-weight: 700;
+	}
+	
+	.swiper{
+		height: 240rpx;
 	}
 	
 	.search{
@@ -137,4 +779,252 @@
 		padding-bottom: 20rpx;
 		background-color: #0D92FF;
 	}
+	
+	.Merit{
+		display: flex;
+		justify-content: space-around;
+		margin-bottom: 20rpx;
+	}
+	
+	.Merit_icon{
+		width: 90rpx;
+		height: 90rpx;
+		margin: 20rpx;
+		background-color: #007AFF;
+		border-radius: 50%;
+	}
+	
+	.Merit_name{
+		font-size: 24rpx;
+		text-align: center;
+		color: #8A8A8A;
+	}
+	
+	.screen_box{
+		margin: 0 10rpx;
+		display: flex;
+		justify-content: space-between;
+	}
+	
+	.screen_left{
+		display: flex;
+	}
+	
+	.select_item{
+		margin: 0 10rpx;
+		font-size: 24rpx;
+	}
+	
+	.setMeal_box{
+		display: flex;
+	}
+	
+	
+	.setMeal_item{
+		margin: 0 26rpx;
+		font-size: 28rpx;
+	}
+	
+	
+	.label_box{
+		display: flex;
+	}
+	
+	.label_item{
+		padding:14rpx 20rpx;
+		margin: 0 20rpx;
+		background: #F8F8F8;
+		font-size: 24rpx;
+	}
+	
+	.good_all_box{
+		margin-bottom: 30rpx;
+	}
+	
+	.good_item_box{
+		display: flex;
+		margin: 20rpx;
+	}
+	
+	.googImgBox{
+		width: 170rpx;
+		height: 170rpx;
+		border-radius: 10rpx;
+		background: #007AFF;
+		margin-right: 20rpx;
+	}
+	.good_item_describe{
+		flex: 1;
+	}
+	
+	.good_name_mueu{
+		display: flex;
+		justify-content: space-between;
+	}
+	
+	.good_name{
+		font-size: 30rpx;
+		color: #000;
+	}
+	.good_describe{
+		display: flex;
+		margin: 5rpx 0;
+	}
+	
+	.good_score{
+		margin-right: 20rpx;
+		font-size: 28rpx;
+	}
+	
+	.good_sale{
+		font-size: 28rpx;
+	}
+	
+	.good_start_price,.good_distance_time,.good_describe_label_box,.good_taste_label_box{
+		font-size: 24rpx;
+	}
+	
+	.good_performance{
+		width: 100%;
+		display: flex;
+		justify-content: space-between;
+	}
+	
+	.good_describe_label_box{
+		display: flex;
+		
+	}
+	
+	.good_item_describe_label{
+		color: #E19D58;
+		background: #FEF4E9;
+		margin-right: 16rpx;
+		padding: 2rpx 20rpx;
+		border-radius: 1rpx;
+	}
+	
+	.good_item_taste_label_box{
+		color: #EEACAA;
+		border: 1rpx solid #FCFCFC;
+		margin-right: 16rpx;
+		padding: 2rpx 20rpx;
+		border-radius: 1rpx;
+	}
+	
+	.good_taste_label_box{
+		display: flex;
+	}
+	
+	.good_reserve{
+		padding: 10rpx;
+		border: 1rpx solid #999;
+	}
+	
+	
+	
+	.member_box{
+		padding: 0 20rpx;
+		
+	}
+	
+	.ShoppingCart{
+		position: fixed;
+		bottom: 80rpx;
+		right: 40rpx;
+		width: 100rpx;
+		height: 100rpx;
+		background: #ccc;
+		border-radius: 50%;
+	}
+	
+	.bac_img{
+		margin-bottom: 20rpx;
+		padding-bottom: 30rpx;
+		background: #4CD964;
+		border-radius: 10rpx;
+	}
+	
+	.bac_title{
+		display: inline-block;
+		padding:10rpx 52rpx;
+		background: #F8621E;
+		color: #fff;
+		font-size: 24rpx; 
+		margin-left: 22rpx;
+		border-radius:  0 0 10rpx 10rpx;
+		margin-bottom: 10rpx;
+	}
+	
+	.food_box{
+		display: flex;
+		margin-left: 26rpx;
+	}
+	
+	.food_item{
+		border-radius: 10rpx;
+		margin-right: 16rpx;
+		overflow: hidden;
+	}
+	
+	
+	
+	.food_img{
+		width: 340rpx;
+		height: 120rpx;
+		background: #ccc;
+	}
+	
+	.food_describe{
+		background: #EAF6DE;
+		padding: 10rpx;
+	}
+	
+	.food_name{
+		margin: 10rpx;
+	}
+	
+	.price{
+		margin: 10rpx;
+	}
+	
+	.distance{
+		display: flex;
+		justify-content: space-between;
+		margin: 10rpx;
+	}
+	
+	.distance view{
+		font-size: 24rpx;
+	}
+	
+	.shop_box{
+		display: flex;
+		justify-content: space-between;
+	}
+	
+	.shop{
+		display: flex;
+	}
+	
+	.good_mune_box{
+		display: flex;
+	}
+	
+	.good_item_mune_box{
+		margin: 0 30rpx;
+	}
+	
+	.good_mune_img{
+		width: 170rpx;
+		height: 170rpx;
+		background: #007AFF;
+		
+	}
+	
+	.good_mune_price{
+		font-size: 26rpx;
+	}
+	
+	
+	
 </style>
--- pages/order/order.vue
+++ pages/order/order.vue
@@ -1,6 +1,48 @@
 <template>
 	<view>
+		<view :style="{'height':navigation.height+'px',
+						'paddingTop': navigation.top + 'px',
+						'paddingBottom':'10rpx',
+						'backgroundColor':'#0D92FF',
+						'color':'#fff'}" >
+			<view :style="{'height':navigation.height+'px'}" class="topNavigation">
+				我的订单
+			</view>
+		</view>
 		
+		<view class="screen_select">
+			<view class="screen_select_name">
+				全部
+			</view>
+			<view class="screen_select_name">
+				全部
+			</view>
+			<view class="screen_select_name">
+				全部
+			</view>
+			<view class="screen_select_name">
+				全部
+			</view>
+		</view>
+		
+		<view class="good_order_box">
+			<view class="good_item_order">
+				<view class="good_item_top">
+					<view class="item_order_box">
+						<view class="good_logo">
+							logo
+						</view>
+						<view class="good_name">
+							店铺名称
+						</view>
+					</view>
+					<view class="order_statce">
+						待支付
+					</view>
+					
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,8 +50,11 @@
 	export default {
 		data() {
 			return {
-				
+				navigation:''
 			}
+		},
+		created() {
+			this.navigation = this.$store.getters.getNavigation
 		},
 		methods: {
 			
@@ -18,5 +63,30 @@
 </script>
 
 <style>
-
+	.topNavigation{
+		font-size: 40rpx;
+		margin-left: 40rpx;
+		font-weight: 800;
+	}
+	
+	.screen_select{
+		display: flex;
+		background: #0D92FF;
+	}
+	
+	.screen_select_name{
+		font-size: 36rpx;
+		padding:20rpx 40rpx;
+		color: #fff;
+	}
+	
+	.good_item_order{
+		margin: 20rpx;
+	}
+	
+	.good_item_top{
+		display: flex;
+		justify-content: space-between;
+	}
+	
 </style>
